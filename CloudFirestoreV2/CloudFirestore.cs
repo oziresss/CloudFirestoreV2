23e6fbc0cbce14eb398eb0688593a5e3ee0a5706
Ozires Alves Da Rocha Filho <ozires.rocha@gpssa.com.br>
2024-07-16 06:09:01 -0300

    Add project files.

---
+++ CloudFirestoreV2/CloudFirestore.cs
@@ -0,0 +1,30 @@
+﻿using Google.Cloud.Firestore;
+
+namespace CloudFirestoreV2
+{
+    public class CloudFirestore
+    {
+        public async Task BuscarDados()
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "service-account-cloudfirestore.json");
+            Console.WriteLine($"CRIAÇÃO CONCLUÍIDA DA VARIÁVEL DE AMBIENTE.");
+
+            FirestoreDb firestoreDb = FirestoreDb.Create("nome_projeto");
+            CollectionReference documentosRef = firestoreDb.Collection("nome_colecao");
+            Query query = documentosRef.WhereEqualTo("Cpf", "00000000000");
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
+            {
+                Dictionary<string, object> documento = documentSnapshot.ToDictionary();
+
+                foreach (KeyValuePair<string, object> parametro in documento)
+                {
+                    Console.WriteLine("{0}: {1}", parametro.Key, parametro.Value);
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ CloudFirestoreV2/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var cloudFirestore = new CloudFirestoreV2.CloudFirestore();
+
+        var dataInicio = DateTime.Now;
+
+        await cloudFirestore.BuscarDados();
+
+        var dataFim = DateTime.Now;
+
+        Console.WriteLine($"TEMPO DE EXECUÇÃO {dataFim.Minute - dataInicio.Minute} MINUTOS.");
+    }
+}
